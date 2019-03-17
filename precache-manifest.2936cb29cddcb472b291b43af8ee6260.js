e371ae404080772f206b7b7e457db8ca588e5350
alfredomota <alfredolaureta@hotmail.com>
2019-03-17 18:41:25 +0000

    Updates

---
+++ precache-manifest.2936cb29cddcb472b291b43af8ee6260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a14842fe0db112ca01f8",
+    "url": "/ZtMRoboFriends/static/css/main.61767fa5.chunk.css"
+  },
+  {
+    "revision": "a14842fe0db112ca01f8",
+    "url": "/ZtMRoboFriends/static/js/main.d9f46f10.chunk.js"
+  },
+  {
+    "revision": "3e10765ff31d76f48117",
+    "url": "/ZtMRoboFriends/static/js/runtime~main.45eb83cd.js"
+  },
+  {
+    "revision": "49236b9862bdf2c1d6e4",
+    "url": "/ZtMRoboFriends/static/css/2.a7ddb1f1.chunk.css"
+  },
+  {
+    "revision": "49236b9862bdf2c1d6e4",
+    "url": "/ZtMRoboFriends/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ZtMRoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "267b2b2badce14a4faf1d8a01efffbe4",
+    "url": "/ZtMRoboFriends/index.html"
+  }
+];
